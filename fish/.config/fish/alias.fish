3c7312f7dfdfc52fd2df5382a7d3d0d9878e6197
Nathanael <nathanael@bynder.com>
2019-12-27 00:13:40 +0100

    config update

--- fish/.config/fish/alias.fish
+++ fish/.config/fish/alias.fish
@@ -1,4 +1,6 @@
 alias vim 'nvim'
 
+alias tmux 'tmux -u'
+
 alias gcc '$BREW_PREFIX/bin/gcc-8'
 alias g++ '$BREW_PREFIX/bin/g++-8'
--- fish/.config/fish/env.fish
+++ fish/.config/fish/env.fish
@@ -1,5 +1,8 @@
 # gcc-arm toolchain
-set -g fish_user_paths $HOME/opt/gcc-arm-none-eabi-7-2018-q2-update/bin $fish_user_path
+set -g fish_user_paths $HOME/opt/gcc-arm-none-eabi-7-2018-q2-update/bin $fish_user_paths
+
+# binaries
+set -g fish_user_paths $HOME/utils/binaries $fish_user_paths
 
 # brew prefix
 set -g BREW_PREFIX /usr/local 
