76a2d2285136436fa5edc277b5e6baa35b777e5b
Nlte <n.leaute@gmail.com>
2020-03-18 23:27:49 +0100

    eshell emacs

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Nlte"
+      user-mail-address "")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -19,7 +19,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 14))
+(setq doom-font (font-spec :family "Source code pro" :size 12))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -51,3 +51,12 @@
 ;;
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
+
+;; keyboard conf
+(when (eq system-type 'darwin) ;; mac specific settings
+  ;;https://www.emacswiki.org/emacs/MetaKeyProblems
+  (setq mac-option-key-is-meta 'nil)
+  (setq mac-command-key-is-meta t)
+  (setq mac-option-modifier 'nil)
+  (setq mac-command-modifier 'meta)
+  )
